64f6d9719e6475fcbda7476f91d546c4208c25ca
maxwbuckley <maxwbuckley@gmail.com>
2016-11-02 21:27:53 +0000

    Thoroughly improved the sparse_matrix code. Added dense constructor.

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -8,8 +8,36 @@ class NonConformableException(Exception):
 
 
 class SparseMatrix(object):
-  def __init__(self, sparse_matrix_proto):
+  def __init__(self, sparse_matrix_proto=None, dense_matrix=None):
     """Initialize Sparse Matrix.
+
+    Args:
+      sparse_matrix_proto: A sor_pb2.SparseMatrix proto.
+      dense_matrix: A list of lists with only numerical entries.
+    """
+    if not (sparse_matrix_proto or dense_matrix):
+      raise Exception("Need to pass a proto or matrix to constructor")
+    if sparse_matrix_proto and dense_matrix:
+      raise Exception("Both should not be submitted")
+    if sparse_matrix_proto:
+      self.from_proto(sparse_matrix_proto)
+    else:
+      self.from_dense_matrix(dense_matrix)
+  
+  def from_dense_matrix(self, dense_matrix):
+    """Construct from dense matrix.
+
+    Args:
+      dense_matrix: A list of lists with only numerical entries.
+    """
+    # Needs validation
+    self.columns = len(dense_matrix[0])
+    self.rows = len(dense_matrix[0])
+    self.rowStart, self.cols, self.vals = self._get_csr_structure(
+      dense_matrix=dense_matrix)
+
+  def from_proto(self, sparse_matrix_proto):
+    """Construct from sparse matrix proto.
 
     Args:
       sparse_matrix_proto: A sor_pb2.SparseMatrix proto.
@@ -20,23 +48,50 @@ class SparseMatrix(object):
     self.rowStart, self.cols, self.vals = self._get_csr_structure(
       sparse_matrix_proto.values)
 
-  def _get_csr_structure(self, sparse_value_protos):
-    temp_list = []
-    for value in sparse_value_protos:
-      temp_list.append((value.row_index, value.column_index, value.value))
+
+  def _convert_dense_matrix_to_tuple_list(self, dense_matrix):
+    """Convert a dense matrix into a list of its non 0 valued tuples.
+    Args:
+      dense_matrix: A list of lists with only numerical entries.
+    Returns:
+      A list of tuples of the form row index, col_indes, value.
+    """
+    tuple_list = []
+    for i in range(len(dense_matrix)):
+      for j in range(len(dense_matrix[0])):
+        if dense_matrix[i][j] != 0:
+          tuple_list.append((i, j, dense_matrix[i][j]))
+    return tuple_list
+
+  def _convert_proto_to_tuple_list(self, sparse_value_proto):
+    tuple_list = []
+    for value in sparse_value_proto:
+      tuple_list.append((value.row_index, value.column_index, value.value))
+    return tuple_list
+
+  def _get_csr_structure(self, sparse_value_proto=None, dense_matrix=None):
+    # Need some checking
+    if sparse_value_proto is not None:
+      temp_list = self._convert_proto_to_tuple_list(sparse_value_proto)
+    elif dense_matrix is not None:
+      temp_list = self._convert_dense_matrix_to_tuple_list(dense_matrix)
     sorted_list = sorted(temp_list, key=lambda element: (element[0], element[1]))
+    print(sorted_list)
     # popping left is O(n). Better to replace with a queue.
     row, col, val = sorted_list.pop(0)
     rowStart = [row]
     cols = [col]
     vals = [val]
-    for row in sorted_list:
-      if row[0] > rowStart[-1]:
-        rowStart.append(row[0])
+    # This does not get returned. Used to keep track.
+    rows = [row]
+    for i, row in enumerate(sorted_list):
+      if row[0] > rows[-1]:
+        rowStart.append(i + 1)
       cols.append(row[1])
       vals.append(row[2])
+      rows.append(row[0])
     # Add sentinel value.
-    rowStart.append(self.rows)
+    rowStart.append(len(vals))
     return rowStart, cols, vals
 
   def __repr__(self):
