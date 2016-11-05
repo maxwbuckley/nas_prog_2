d74847fd46dbec6e710a7d35ce4312d63900498e
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2016-11-05 17:06:41 +0000

    Add validation tests

--- validation_test.py
+++ validation_test.py
@@ -18,13 +18,40 @@ class ValidationTest(unittest.TestCase):
         validation.ValidationError, validation.ValidateVectorProto, vector)
 
   def testValidateSparseMatrixProto_Success(self):
-    pass
+    matrix_a_proto = sor_pb2.SparseMatrix(
+      matrix_name='a', row_count=3, column_count=3)
+    for i in range(0, 3):
+      value = matrix_a_proto.values.add()
+      value.row_index = i
+      value.column_index = i
+      value.value = (i + 1) * 2
 
   def testValidateSparseMatrixProto_OutOfBounds(self):
-    pass
+    matrix_a_proto = sor_pb2.SparseMatrix(
+      matrix_name='a', row_count=3, column_count=3)
+    for i in range(0, 4):
+      value = matrix_a_proto.values.add()
+      value.row_index = i
+      value.column_index = i
+      value.value = (i + 1) * 2
+    
+    self.assertRaises(validation.ValidationError,
+                      validation.ValidateSparseMatrixProto, matrix_a_proto)
 
   def testValidateSparseMatrixProto_Collisions(self):
-    pass
+    matrix_a_proto = sor_pb2.SparseMatrix(
+      matrix_name='a', row_count=3, column_count=3)
+    for i in range(0, 3):
+      value = matrix_a_proto.values.add()
+      value.row_index = i
+      value.column_index = i
+      value.value = (i + 1) * 2
+    value.row_index = 1
+    value.column_index = 1
+    value.value = 1
+
+    self.assertRaises(validation.ValidationError,
+                      validation.ValidateSparseMatrixProto, matrix_a_proto)
 
   def testValidateNumberList_Success(self):
     self.assertTrue(validation.ValidateNumberList([1, 2, 4, 6.5]))
