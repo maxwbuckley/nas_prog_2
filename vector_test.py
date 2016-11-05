42d5f89c7ca77dd48cc4a307ec06519992de0756
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2016-11-05 17:36:36 +0000

    Define tests to be created

--- vector_test.py
+++ vector_test.py
@@ -2,4 +2,15 @@
 """Unit tests associated with validation.py."""
 from proto_genfiles.protos import sor_pb2
 import unittest
-import validation
+import vector
+
+class VectorTest(unittest.TestCase):
+
+  def testVector_FromProto(self):
+      pass
+
+  def testVector_FromNumberList(self):
+      pass
+
+if __name__ == '__main__':
+  unittest.main()
