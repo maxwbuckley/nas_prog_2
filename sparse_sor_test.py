e439ce5353f8c71a3500195ae161f26e6ef6a51e
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2016-11-06 20:10:23 +0000

    Test for correct results for sparse_sor_solver

--- sparse_sor_test.py
+++ sparse_sor_test.py
@@ -6,6 +6,9 @@ import sparse_matrix
 from proto_genfiles.protos import sor_pb2
 import unittest
 
+def almost_equal(value_1, value_2, accuracy = 10**4):
+  return abs(value_1 - value_2) < accuracy
+
 class SparseSorSolverTest(unittest.TestCase):
 
   def testSparseSorSolver_MatrixProto(self):
@@ -36,8 +39,24 @@ class SparseSorSolverTest(unittest.TestCase):
     vector_b = vector.Vector(name="b", number_list=[1, 1, 1])
 
     sparse_sor_solver = sparse_sor.SparseSorSolver(
+        matrix_a, vector_b, 10, .0001, 1.25)
+    print(sparse_sor_solver)  
+  
+  def testSparseSorSolver_SolvedExample(self):
+    matrix_a = sparse_matrix.SparseMatrix(dense_matrix=
+        [[3, -1, 1],
+         [-1, 3, -1],
+         [1, -1, 3]])
+
+    vector_b = vector.Vector(name="b", number_list=[-1, 7, -7])
+
+    sparse_sor_solver = sparse_sor.SparseSorSolver(
         matrix_a, vector_b, 10, .0001, 1.0)
-    print(sparse_sor_solver)
+    
+    expected = [1, 2, -2]
+    
+    self.assertTrue(all(
+      almost_equal(*values) for values in zip(expected, sparse_sor_solver.x)))
 
 if __name__ == '__main__':
   unittest.main() 
