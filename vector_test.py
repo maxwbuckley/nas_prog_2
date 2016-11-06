181abe9eb7bbd13923b94b0c8edb52dd4b27aaf7
rcreagh <rcreagh@rcreagh-macbookair.roam.corp.google.com>
2016-11-06 17:12:52 +0000

    Add unit test to vector_test

--- vector_test.py
+++ vector_test.py
@@ -7,10 +7,25 @@ import vector
 class VectorTest(unittest.TestCase):
 
   def testVector_FromProto(self):
-      pass
-
+    vector_a_proto = sor_pb2.Vector(vector_name = "a", length = 5,
+                                    values = [1, 2, 3, 4, 5])        
+    vector_a = vector.Vector(vector_proto=vector_a_proto)
+                
+    expected = vector.Vector(number_list=[1, 2, 3, 4, 5])
+      
+    self.assertEqual(expected.values, vector_a.values)
+      
   def testVector_FromNumberList(self):
-      pass
+    expected = [1, 2, 3, 4, 5]
+    vector_a = vector.Vector(number_list=expected)
+    
+    self.assertEqual(expected, vector_a.values)
+      
+  def testVector_WithZeroValues(self):
+    expected = [0, 1, 2, 3, 4]
+    vector_a = vector.Vector(number_list=expected)
+    
+    self.assertEqual(expected, vector_a.values)
 
 if __name__ == '__main__':
   unittest.main()
